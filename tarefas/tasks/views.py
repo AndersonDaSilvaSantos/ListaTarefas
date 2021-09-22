3fe7be8fe11f429d7366cbea990def873d2ff62e
Anderson <anderson_silva05@hotmail.com>
2021-09-22 20:22:45 -0300

    teste

---
+++ tarefas/tasks/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def helloWorld(request):
+    return HttpResponse('Hello World!')
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})    
