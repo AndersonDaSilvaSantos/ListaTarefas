0b1aa90a5be40a5b4e762994c8b4d5999beb8008
Anderson <anderson_silva05@hotmail.com>
2021-10-05 21:26:13 -0300

    css e js

--- tarefas/tasks/urls.py
+++ tarefas/tasks/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('helloworld/', views.helloworld),
+    path('helloworld/', views.helloworld), 
     path('', views.tasklist, name='task-list'),
     path('yourname/<str:name>', views.yourname, name='yourname'),
 ]
